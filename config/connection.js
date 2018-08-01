2ec73ed409717cacbb8efe9c86e85f659551faa9
gkwazy <noreply>
2018-08-01 00:29:29 -0600

    closer

--- config/connection.js
+++ config/connection.js
@@ -1,32 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//     host: "us-cdbr-iron-east-01.cleardb.net",
-//     database: "heroku_3f24df265732b16",
-//     port: 3306,
-//     user: "b2269d8746c617",
-//     password: "aefb17a8"
-// });
-
-// // Make connection.
-// connection.connect(function (err) {
-//     if (err) {
-//         console.error("error connecting: " + err.stack);
-//         return;
-//     }
-//     console.log("connected as id " + connection.threadId);
-// });
-
-// // Export connection for our ORM to use.
-// module.exports = connection;
-
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "us-cdbr-iron-east-01.cleardb.net",
+    database: "heroku_3f24df265732b16",
     port: 3306,
-    user: "root",
-    password: "root",
-    database: "burger_db"
+    user: "b2269d8746c617",
+    password: "aefb17a8"
 });
 
 // Make connection.
@@ -40,3 +20,23 @@ connection.connect(function (err) {
 
 // Export connection for our ORM to use.
 module.exports = connection;
+
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "root",
+//     database: "burger_db"
+// });
+
+// // Make connection.
+// connection.connect(function (err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
+
+// // Export connection for our ORM to use.
+// module.exports = connection;
